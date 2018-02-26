f697ae79d0385d2363b95ee7b75746829d70f1f3
conradletelier <conrad@letelier.email>
2018-02-26 21:23:45 +0100

    project

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'softy',
+			tags: [],
+			salt: '5cf35826540c48e5d16b37622c0053d4',
+			hash: 'c28aabaaa44a504c5d80a97f31c8e6ed410690a2316985fffa927cfb36448473',
+			user_created_timestamp: 1519590552136
+		}
+	]
+})
---
+++ cms/index.js
@@ -0,0 +1,54 @@
+({
+	first_slide: {
+		header: 'Enduro Bands!',
+		tagline: 'All of our bands in one place!',
+		call_to_action: 'Scroll down to see what\'s up'
+	},
+	bands: {
+		list_of_bands: [
+			{
+				name: 'Nirvana',
+				genre: 'Rock',
+				label: 'Sub Pop',
+				picUrl: 'http://www.trollradio.gr/wp-content/uploads/2018/01/nirvana.jpg'
+			},
+			{
+				name: 'Deftones',
+				genre: 'Rock',
+				label: 'Warner Bros Records',
+				picUrl: 'http://fortemag.com.au/wp-content/uploads/Deftones-snow.jpg'
+			}
+		]
+	},
+	labels: {
+		list_of_labels: [
+			{
+				name: 'Warner Bros Records',
+				list_of_bands: [
+					{
+						name: 'Deftones'
+					}
+				]
+			},
+			{
+				name: 'Sub Pop',
+				list_of_bands: [
+					{
+						name: 'Nirvana'
+					}
+				]
+			},
+			{
+				name: 'GodModeLabel',
+				list_of_bands: [
+					{
+						name: 'Nirvana'
+					},
+					{
+						name: 'Deftones'
+					}
+				]
+			}
+		]
+	}
+})
